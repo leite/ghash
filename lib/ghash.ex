2b21275406ee166e1ca5d0818f4665ab39a5ebbe
leite <xxleite@gmail.com>
2019-06-19 15:10:46 -0300

    split cond into multiple functions, get bounds

--- lib/ghash.ex
+++ lib/ghash.ex
@@ -32,17 +32,21 @@ defmodule Ghash do
       228644876657266
 
   """
-  def encode lat, lon \\ 0, binary \\ false, depth \\ 50 do
-    cond do
-      is_integer lat ->
-        encode_string lat
-      is_float(lon) and binary ->
-        encode lat, lon, depth, -90.0, 90.0, -180.0, 180.0
-      is_float(lon) and !binary ->
-        encode(lat, lon, depth, -90.0, 90.0, -180.0, 180.0) |> encode_string
-      true ->
-        nil
-    end
+  def encode lat, lon \\ [], binary \\ false, depth \\ 50
+  def encode(0, list, _binary, _depth) when is_list list do
+    to_string list
+  end
+
+  def encode(bin, list, _binary, _depth) when is_integer(bin) and is_list(list) do
+    encode bin >>> 5, [@b32[rem(bin, 0x20)] | list]
+  end
+
+  def encode(lat, lon, true, depth) when is_float(lat) and is_float(lon) do
+    encode lat, lon, depth, -90.0, 90.0, -180.0, 180.0
+  end
+
+  def encode(lat, lon, false, depth) when is_float(lat) and is_float(lon) do
+    encode(lat, lon, depth, -90.0, 90.0, -180.0, 180.0) |> encode
   end
 
   @doc """
@@ -58,15 +62,34 @@ defmodule Ghash do
     String.codepoints(hash) |> decode_binary
   end
 
-  defp encode_string integer, string \\ []
-  defp encode_string 0, string do
-    to_string string
+  @doc """
+  Get geohash bounds
+
+  ## Examples
+
+      iex> Ghash.bounds "6gyf4bf8mk"
+      %{
+        ne: [-23.550497889518738, -46.63329362869263],
+        sw: [-23.550503253936768, -46.63330435752869]
+      }
+
+      iex> Ghash.bounds 228644876657266
+      %{
+        ne: [-23.550497889518738, -46.63329362869263],
+        sw: [-23.550503253936768, -46.63330435752869]
+      }
+
+  """
+  def bounds(hash) when is_bitstring hash do
+    {lo_min, lo_max, la_min, la_max} =
+      String.codepoints(hash)
+      |> bounds(-90.0, 90.0, -180.0, 180.0, true)
+
+    %{sw: [la_min, lo_min], ne: [la_max, lo_max]}
   end
 
-  defp encode_string number, string do
-    index = rem number, 0x20
-
-    encode_string number >>> 5, [@b32[index] | string]
+  def bounds(hash) when is_integer hash do
+    encode(hash) |> bounds
   end
 
   defp decode_binary list, number \\ 0
@@ -99,6 +122,43 @@ defmodule Ghash do
       encode lat, lon, depth, la_min, la_max, lo_min, lo_max, total, combined, !even
     else
       combined
+    end
+  end
+
+  defp bounds [char], la_min, la_max, lo_min, lo_max, even do
+    get_bounds @b32[char], la_min, la_max, lo_min, lo_max, even
+  end
+
+  defp bounds [char | tail], la_min, la_max, lo_min, lo_max, even do
+    {lo_min, lo_max, la_min, la_max} =
+      get_bounds @b32[char], la_min, la_max, lo_min, lo_max, even
+
+    bounds tail, la_min, la_max, lo_min, lo_max, !even
+  end
+
+  defp get_bounds char_index, la_min, la_max, lo_min, lo_max, even, n \\ 4 do
+    bit = char_index >>> n &&& 1
+
+    {lo_min, lo_max, la_min, la_max} = result = if even do
+      mid = (lo_min + lo_max) / 2
+      if bit == 1 do
+        {mid, lo_max, la_min, la_max}
+      else
+        {lo_min, mid, la_min, la_max}
+      end
+    else
+      mid = (la_min + la_max) / 2
+      if bit == 1 do
+        {lo_min, lo_max, mid, la_max}
+      else
+        {lo_min, lo_max, la_min, mid}
+      end
+    end
+
+    if n == 0 do
+      result
+    else
+      get_bounds char_index, la_min, la_max, lo_min, lo_max, !even, n - 1
     end
   end
 end
