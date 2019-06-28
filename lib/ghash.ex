610910438237aaaf65ef6a96b5b5ec0e604677fe
leite <xxleite@gmail.com>
2019-06-28 18:52:45 -0300

    ranges of hashs

--- lib/ghash.ex
+++ lib/ghash.ex
@@ -6,7 +6,13 @@ defmodule Ghash do
   Documentation for Ghash.
   """
 
-  @b32 ~h(0 1 2 3 4 5 6 7 8 9 b c d e f g h j k m n p q r s t u v w x y z)
+  @b32  ~h(0 1 2 3 4 5 6 7 8 9 b c d e f g h j k m n p q r s t u v w x y z)
+
+  # 4x8 prxz, nqwy, ...
+  @even ~h(p r x z n q w y j m t v h k s u 5 7 e g 4 6 d f 1 3 9 c 0 2 8 b)
+
+  # 8x4 bcfguvyz, 89destwx, ...
+  @odd  ~h(b c f g u v y z 8 9 d e s t w x 2 3 6 7 k m q r 0 1 4 5 h j n p)
 
   @doc """
   Encode geohash
@@ -99,6 +105,48 @@ defmodule Ghash do
     encode(hash) |> bounds
   end
 
+  @doc """
+  Get range of hashs
+
+  ## Examples
+
+    iex> Ghash.range "6u4-gx"
+    [
+      "6u4", "6u5", "6uh", "6uj", "6un", "6up",
+      "6gf", "6gg", "6gu", "6gv", "6gy", "6gz",
+      "6gd", "6ge", "6gs", "6gt", "6gw", "6gx"
+    ]
+
+    iex> Ghash.range "7p9", "7pn"
+    [
+      "7p9", "7pd", "7pe", "7ps", "7pt", "7pw",
+      "7p3", "7p6", "7p7", "7pk", "7pm", "7pq",
+      "7p1", "7p4", "7p5", "7ph", "7pj", "7pn"
+    ]
+
+    iex> Ghash.range "7p9", "pn"
+    nil
+
+  """
+  def range from_to do
+    [from, to] = String.split from_to, "-"
+    {first, _} = String.split_at from, String.length(from) - String.length(to)
+
+    range from, "#{first}#{to}"
+  end
+
+  def range from, to do
+    cond do
+      String.length(from) == String.length(to) ->
+        get_boundaries(from, to)
+        |> range_recusive
+        |> List.flatten
+        |> Enum.reverse
+      true ->
+        nil
+    end
+  end
+
   defp decode_binary list, number \\ 0
   defp decode_binary [single], number do
     number ||| @b32[single]
@@ -167,5 +215,78 @@ defmodule Ghash do
     else
       get_bounds char_index, la_min, la_max, lo_min, lo_max, !even, n - 1
     end
+  end
+
+  defp get_boundaries from, to do
+    get_boundaries(String.codepoints(from), String.codepoints(to), 8, [])
+    |> Enum.reverse
+  end
+
+  defp get_boundaries [from], [to], 8, results do
+    [calc_boundary(@odd[from], @odd[to], 8) | results]
+  end
+
+  defp get_boundaries [from], [to], 4, results do
+    [calc_boundary(@even[from], @even[to], 4) | results]
+  end
+
+  defp get_boundaries [from | t_from], [to | t_to], 8, results do
+    get_boundaries t_from, t_to, 4, [calc_boundary(@odd[from], @odd[to], 8) | results]
+  end
+
+  defp get_boundaries [from | t_from], [to | t_to], 4, results do
+    get_boundaries t_from, t_to, 8, [calc_boundary(@even[from], @even[to], 4) | results]
+  end
+
+  defp calc_boundary from, to, column do
+    {rem(from, column) , div(from, column), rem(to, column), div(to, column)}
+  end
+
+  defp range_recusive a, b \\ {true, true, true, true}, c \\ true, d \\ ""
+  defp range_recusive [pos], limits, true, parent do
+    {col_range, row_range} = get_limits pos, limits, 7, 3
+
+    for r <- row_range, c <- col_range, reduce: [] do
+      acc -> ["#{parent}#{@odd[(r * 8) + c]}" | acc]
+    end
+  end
+
+  defp range_recusive [pos], limits, false, parent do
+    {col_range, row_range} = get_limits pos, limits, 3, 7
+
+    for r <- row_range, c <- col_range, reduce: [] do
+      acc -> ["#{parent}#{@even[(r * 4) + c]}" | acc]
+    end
+  end
+
+  defp range_recusive [{x, y, xx, yy} = pos | tail], limits, true, parent do
+    {col_range, row_range} = get_limits pos, limits, 7, 3
+
+    for r <- row_range, c <- col_range, reduce: [] do
+      acc -> [
+        range_recusive(
+          tail, {x == c, y == r, xx == c, yy == r}, false, "#{parent}#{@odd[(r * 8) + c]}"
+        ) | acc
+      ]
+    end
+  end
+
+  defp range_recusive [{x, y, xx, yy} = pos | tail], limits, false, parent do
+    {col_range, row_range} = get_limits pos, limits, 3, 7
+
+    for r <- row_range, c <- col_range, reduce: [] do
+      acc -> [
+        range_recusive(
+          tail, {x == c, y == r, xx == c, yy == r}, true, "#{parent}#{@even[(r * 4) + c]}"
+        ) | acc
+      ]
+    end
+  end
+
+  defp get_limits {x, y, xx, yy}, {col, row, col_2, row_2}, col_size, row_size do
+    {
+      (if col, do: x, else: 0)..(if col_2, do: xx, else: col_size),
+      (if row, do: y, else: 0)..(if row_2, do: yy, else: row_size)
+    }
   end
 end
